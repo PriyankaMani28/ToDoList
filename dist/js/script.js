2c149a2e0b551c15a14e277989f7728876ae043b
PriyankaMani28 <88648858+PriyankaMani28@users.noreply.github.com>
2024-09-08 16:07:56 +0530

    File Upload

---
+++ dist/js/script.js
@@ -0,0 +1,40 @@
+const inputTask = document.querySelector('#inputTask');
+const btn = document.querySelector('#btn');
+const listContainer = document.querySelector('#listContainer');
+
+btn.addEventListener('click', function () {
+    if (inputTask.value == '') {
+        alert('You must write something!')
+    } else {
+        let li = document.createElement('li');
+        li.innerHTML = inputTask.value;
+        listContainer.appendChild(li);
+
+        let span = document.createElement('span');
+        span.innerHTML = '\u00d7';
+        li.appendChild(span);
+    }
+
+    inputTask.value = '';
+    saveData();
+})
+
+listContainer.addEventListener('click', function (e) {
+    if (e.target.tagName === 'LI') {
+        e.target.classList.toggle('checked');
+        saveData();
+    } else if (e.target.tagName === 'SPAN') {
+        e.target.parentElement.remove();
+        saveData();
+    }
+}, false);
+
+function saveData() {
+    localStorage.setItem('data', listContainer.innerHTML);
+}
+
+function showTask() {
+    listContainer.innerHTML = localStorage.getItem('data');
+}
+
+showTask();
